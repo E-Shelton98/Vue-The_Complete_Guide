818b6589a9c226b0822211d58e3f3f2fc79fde6f
E-Shelton98 <erik.shelton92@gmail.com>
2023-03-11 23:23:54 -0500

    completed lecture 47.Lists & Keys

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,18 +1,18 @@
 const app = Vue.createApp({
   data() {
     return {
-      enteredGoalValue: '',
       goals: [],
+      enteredGoalValue: ''
     }
   },
   methods: {
     addGoal() {
       this.goals.push(this.enteredGoalValue)
     },
-    removeGoal(idx) {
-      this.goals.splice(idx, 1)
-    },
-  },
+    removeGoal(index) {
+      this.goals.splice(index, 1)
+    }
+  }
 })
 
 app.mount('#user-goals')
